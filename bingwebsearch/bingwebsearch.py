c8c6a8939464a78f5a29bdad50b65639f3719a4f
Sam Edwardes <edwardes.s@gmail.com>
2022-02-15 08:09:19 -0800

    Added starter models and search function

--- bingwebsearch/bingwebsearch.py
+++ bingwebsearch/bingwebsearch.py
@@ -1,7 +1,37 @@
 from .models import ApiResponse
+from typing import List
 
 
 def bingwebsearch(
-    q: str
+    q: str,
+    count: int = 10,
+    response_filter: List[str] = ['Webpages'],
+    mkt: str = 'en-CA',
+    subscription_key: str = None,
+    endpoint: str = "https://api.bing.microsoft.com/v7.0/search"
 ) -> ApiResponse:
-    ...
+
+    # Check environment variables.
+    if subscription_key is None:
+        subscription_key = os.getenv('BING_SEARCH_V7_SUBSCRIPTION_KEY')
+
+    # Set up query parameters and headers.
+    params = {
+        "q": q,
+        "count": count,
+        "responseFilter": response_filter,
+        "mkt": mkt
+    }
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key
+    }
+    
+    # Execute the query
+    r = requests.get(endpoint, headers=headers, params=params)
+
+    # Parse data with Pydantic.
+    api_response = ApiResponse(**r.json())
+    api_response._response = r
+
+    return api_response
